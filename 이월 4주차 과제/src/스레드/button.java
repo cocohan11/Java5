1ebbf2a6dc95402fa5349a45245cf49a5f5a4cb9
한 <rlagksdl96@naver.com>
2021-05-10 12:32:55 +0900

    Initial commit

---
+++ 이월 4주차 과제/src/스레드/button.java
@@ -0,0 +1,28 @@
+package 스레드;
+
+public class button {
+
+    public boolean 배경onOff;
+    public boolean 배경next;
+    public boolean 키보드onOff;
+    public boolean 좀비등장onOff;
+    public boolean 전투onOff;
+    public String 당신이름;
+    public int 어떤물약;
+    public String 엔터skip = "abc";
+    public int 사냥 = 0;  //null값 나와서 값 넣어줌.
+    public int 사냥123 = 99;
+    public int 아이템공격 = 0;
+    public int 싸움_도망 = 99;
+
+    public String 사망후생존자체력 = "어쩌고";
+    public String str = "치료가필요해";
+    public String num = "1";
+    public int 레벨업공격력업;
+    public int 레벨업피통업;
+
+    public int 생공격력초기화;
+    public boolean 공격실패;
+
+
+}
